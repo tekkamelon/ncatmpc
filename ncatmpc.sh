7e9d512c3478e1be9a0e51ce8eddb80dc60a37b1
tekkamelon <type2guntank@gmail.com>
2023-02-14 23:26:41 +0900

    コメントの追加

--- ncatmpc.sh
+++ ncatmpc.sh
@@ -6,14 +6,18 @@ port=$(echo "${MPD_PORT}" | grep . || echo "6600")
 arg1=$(echo "${1}" | grep . || echo "status")
 arg2=$(echo "${2}" | grep . || echo "")
 
+# 引数を出力
 echo "${arg1}" "${arg2}" | 
 
+# 対応する引数があるかを確認
 grep -F -e "playlist" -e "listall" -e "status" -e "play" -e "lsplaylists" |
 
 awk '{
-
+	
+	# "status"があれば真,無ければ偽
 	if($1 == "status"){
-
+		
+		# 真の場合は受け取った文字列と"close"を出力
 		print $0
 	
 		print "close"
@@ -22,6 +26,7 @@ awk '{
 
 	else{
 
+		# 偽の場合は"lsplaylists"を"listplaylists"に置換
 		sub("lsplaylists" , "listplaylists")
 
 		print $0
@@ -34,5 +39,6 @@ awk '{
 
 }' |
 
+# ncに文字列を渡し,"OK"以外を出力
 nc "${host}" "${port}" | grep -F -v "OK"
 
